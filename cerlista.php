3a5ecd8157a009736f3674e6441d629d7485f6b7
Luis Espinoza <luis.manuel.espinoza.estrada@gmail.com>
2020-04-15 15:02:56 -0600

    Certificado

--- cerlista.php
+++ cerlista.php
@@ -119,6 +119,7 @@ Menu
       <th scope="col">Apellido</th>
       <th scope="col">Correo</th>
       <th></th>
+      <th></th>
     </tr>
   </thead>
   <tbody>
@@ -135,6 +136,9 @@ Menu
       <td><?php echo $value["nombre"]; ?></td>
       <td><?php echo $value["apellido"]; ?></td>
       <td><?php echo $value["correo"]; ?></td>
+      <td>  
+      <a><i class="fas fa-certificate" data-toggle="tooltip" data-placement="top" title="Certificado" data-target="#exampleModal"></i></a> 
+      </td>
       <td><a href="cerlista.php?del=<?php echo $value["cedula"];?>"><i class="fas fa-trash-alt" data-toggle="tooltip" data-placement="top" title="Eliminar participante"></i></a></td>
     </tr>
     <?php 
@@ -155,6 +159,29 @@ Menu
 
 </div>
 </div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
